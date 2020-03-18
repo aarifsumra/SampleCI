831ab266867c736648aeefa216ba67856e04028d
Aarif Sumra <aarif.sumra@monstar-lab.com>
2020-03-18 18:28:54 +0900

    initial commit

---
+++ SampleCI/SampleCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCI
+//
+//  Created by Sumra Aarif on 2020/03/18.
+//  Copyright © 2020 Sumra Aarif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleCI/SampleCITests/SampleCITests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleCITests.swift
+//  SampleCITests
+//
+//  Created by Sumra Aarif on 2020/03/18.
+//  Copyright © 2020 Sumra Aarif. All rights reserved.
+//
+
+import XCTest
+@testable import SampleCI
+
+class SampleCITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
